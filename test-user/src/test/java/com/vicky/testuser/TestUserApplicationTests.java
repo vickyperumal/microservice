db22a555449ca1f47f09ce6fa9324e2337cbae26
vicky <vicky@192.168.0.114>
2019-12-17 00:44:16 +0530

    first Service for microservice

---
+++ test-user/src/test/java/com/vicky/testuser/TestUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicky.testuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
