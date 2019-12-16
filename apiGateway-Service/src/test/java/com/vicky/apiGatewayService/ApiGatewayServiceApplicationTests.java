cf2ffb8e31943b0b2cc583ac0054cec1e13b152f
vicky <vicky@192.168.0.114>
2019-12-17 00:42:36 +0530

    created api-gateway

---
+++ apiGateway-Service/src/test/java/com/vicky/apiGatewayService/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicky.apiGatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
